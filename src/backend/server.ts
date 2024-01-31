6720ef75748b115420b12f2482f4bb9c356f5383
Ariel Schwartz <schwartz254@gmail.com>
2024-01-31 20:31:20 +0200

    Adding flash cookies, setting up errors

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -3,14 +3,18 @@ import cookie from "@fastify/cookie";
 import formBody from "@fastify/formBody";
 import staticFiles from "@fastify/static";
 import dotenv from "dotenv";
-import Fastify from "fastify";
+import Fastify, { FastifyRequest } from "fastify";
 import nunjucks from "nunjucks";
 import { z } from "zod";
 
 import { connect, newDb, SqliteSession, SquliteUserRepository } from "./db";
 import { comparePassword, hashPassword } from "./auth";
+import { clearFlashCookie, FLASH_MSG_COOKIE } from "./flash";
+import type { FastifyReply } from "fastify/types/reply";
 
 dotenv.config();
+
+const SESSION_COOKIE = "SESSION_ID";
 
 const environment = process.env.NODE_ENV;
 const cookieSecret = process.env.COOKIE_SECRET;
@@ -46,9 +50,30 @@ type AccountLoginRequest = z.infer<typeof accountLoginRequestSchema>;
     fastify.register(cookie, {
         secret: cookieSecret,
     });
+    fastify.register(clearFlashCookie);
     fastify.register(staticFiles, {
         root: path.join(__dirname, "../../dist")
     });
+}
+
+function setFlashCookie(reply: FastifyReply, msg: string): void {
+    reply.setCookie(FLASH_MSG_COOKIE, msg, {
+        path: "/",
+    });
+}
+
+function readFlashCookie(request: FastifyRequest): string | undefined {
+    return request.cookies[FLASH_MSG_COOKIE];
+}
+
+function setSessionCookie(reply: FastifyReply, sessionId: string): void {
+    reply.setCookie(SESSION_COOKIE, sessionId, {
+        path: "/",
+    });
+}
+
+function readSessionCookie(request: FastifyRequest): string | undefined {
+    return request.cookies[SESSION_COOKIE];
 }
 
 fastify.get("/", async (request, reply) => {
@@ -67,10 +92,12 @@ fastify.post("/account/signup", async (request, reply) => {
     try {
         requestData = accountCreateRequestSchema.parse(request.body);
     } catch (e) {
+        setFlashCookie(reply, "There was an erro processing your request");
         return await reply.redirect("/signup");
     }
 
     if (requestData.agreedToTerms !== "on") {
+        setFlashCookie(reply, "You must agree to the terms of service");
         return await reply.redirect("/signup");
     }
 
@@ -87,8 +114,14 @@ fastify.post("/account/signup", async (request, reply) => {
             hashedPassword,
         };
         const user = await userRepository.create(newUser);
+
+        const sessions = new SqliteSession(db);
+        const sessionId = await sessions.create(user.id);
+        setSessionCookie(reply, sessionId);
+
         return await reply.redirect("/welcome");
     } catch (e) {
+        setFlashCookie(reply, "That account already exists");
         return await reply.redirect("/signup");
     }
 });
@@ -105,6 +138,7 @@ fastify.post("/account/signin", async (request, reply) => {
     try {
         requestData = accountLoginRequestSchema.parse(request.body);
     } catch (e) {
+        setFlashCookie(reply, "There was an error processing your request");
         return await reply.redirect("/signin");
     }
 
@@ -113,14 +147,22 @@ fastify.post("/account/signin", async (request, reply) => {
     try {
         const user = await userRepository.findByEmail(requestData.email);
         if (user === undefined) {
+            setFlashCookie(reply, "Invalid login credentials");
             return await reply.redirect("/signin");
         }
         const passwordMatches = await comparePassword(requestData.password, user.hashedPassword);
         if (!passwordMatches) {
+            setFlashCookie(reply, "Invalid login credentials.");
             return await reply.redirect("/signin");
         }
+
+        const sessions = new SqliteSession(db);
+        const sessionId = await sessions.create(user.id);
+        setSessionCookie(reply, sessionId);
+
         return await reply.redirect("/welcome");
     } catch (e) {
+        setFlashCookie(reply, "Invalid login credentials.");
         return await reply.redirect("/signin");
     }
 });
